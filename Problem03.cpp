a6c8d155cf05466f3877f55e2a74e20c51f540fc
PirateKenny01 <shadab.fardin0234@gmail.com>
2025-11-02 23:27:21 +0600

    Update Problem03.cpp

--- Problem03.cpp
+++ Problem03.cpp
@@ -1,26 +1,49 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <cstdio>
 using namespace std;
 
 void check_Comment(string s)
 {
-    if(s[0] == '/' && s[1] == '/')
+
+    if(s.length() >= 2 && s[0] == '/' && s[1] == '/')
     {
-        cout << "Single Comment Detected" << endl;
+        cout << "Single-Line Comment Detected" << endl;
     }
-    else if(s[0] == '/' && s[1] == '*' && s[s.length()-1] == '/' && s[s.length()-2] == '*')
+    else if(s.length() >= 4 && s[0] == '/' && s[1] == '*' && s[s.length()-2] == '*' && s[s.length()-1] == '/')
     {
-        cout << "Multiple Comment Detected" << endl;
+        cout << "Multi-Line Comment Detected (on a single line)" << endl;
     }
     else
     {
-        cout << "No Comment Detected";
+        cout << "No Comment Detected" << endl;
     }
 }
 
 int main()
 {
     string s;
-    getline(cin , s);
-    check_Comment(s);
+    char choice;
+
+    do {
+        cout << "\nEnter a line of text to check for a comment: ";
+        getline(cin, s);
+
+        check_Comment(s);
+
+        cout << "\nDo you want to check another line? (y/n): ";
+
+        if (!(cin >> choice)) {
+
+            break;
+        }
+
+        char c;
+
+        while ((c = cin.get()) != '\n' && c != EOF) {}
+
+    } while (choice == 'y' || choice == 'Y');
+
+
     return 0;
 }
